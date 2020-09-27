7240baf0eb38fa866d717646a9b28351735e6229
rimijoker <anz19999999999@gmail.com>
2020-09-27 16:17:59 +0530

    Refactor

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,17 +2,18 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   render() {
+    const { onIncrement, onDelete, counter } = this.props;
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.props.onIncrement(this.props.counter)}
+          onClick={() => onIncrement(counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
         <button
-          onClick={() => this.props.onDelete(this.props.counter.id)}
+          onClick={() => onDelete(counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
@@ -21,13 +22,15 @@ class Counter extends Component {
     );
   }
   getBadgeClasses() {
+    const { counter } = this.props;
     let classes = "badge m-2 badge-";
-    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    classes += counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value: count } = this.props.counter;
+    const { counter } = this.props;
+    const { value: count } = counter;
     return count === 0 ? "Zero" : count;
   }
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar
-        </a>
-      </nav>
-    );
-  }
-}
+
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge-pill badge-secondary">{totalCounters}</span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
